abf6f27c5bc479a59225e40cd2be182fc2c0191d
Noel Ortiz <20175580@itla.edu.do>
2021-12-10 03:28:05 -0400

    build campos

--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-tarjeta-credito',
@@ -11,11 +12,34 @@ export class TarjetaCreditoComponent implements OnInit {
     {titulo: 'Noel Ortiz', NumeroTarjeta: '241235426423', FechaExpiracion: '12/23', cvv: '436' },
     {titulo: 'Noelia Ortiz', NumeroTarjeta: '241235426423', FechaExpiracion: '12/24', cvv: '436' },
     {titulo: 'Eridania Sanchez', NumeroTarjeta: '241235426423', FechaExpiracion: '01/30', cvv: '436' }
-  ]
+  ];
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      titulo: [''],
+      NumeroTarjeta: [''],
+      FechaExpiracion: [''],
+      cvv: ['']
+    })
+   }
 
   ngOnInit(): void {
+
+  }
+
+  agregarTarjeta(){
+    console.log(this.form);
+
+    const tarjeta: any = {
+      titulo: this.form.get("titulo")?.value,
+      NumeroTarjeta: this.form.get("NumeroTarjeta")?.value,
+      FechaExpiracion: this.form.get("FechaExpiracion")?.value,
+      cvv: this.form.get("cvv")?.value
+    }
+    this.listaTarjetas.push(tarjeta)
+    this.form.reset();
   }
 
 }
